6ff3feec288625ac7ba58e943ada14d76dfa991a
FatkhanZakia <zakiafattan@.com>
2020-10-15 20:07:42 +0700

    test again again

--- 01-basicfunctionone/basicfunctionone.php
+++ 01-basicfunctionone/basicfunctionone.php
@@ -14,7 +14,7 @@
     echo $namaOrang . ", sudah makan?" . "<br>";
     // echo $namaOrang . ", sudah mandi?" . "<br>";
     // echo $namaOrang . ", sudah tidur?" . "<br>";
-    echo $namaOrang . ", sudah olah raga?" . "<br>";
+    // echo $namaOrang . ", sudah olah raga?" . "<br>";
     echo "Sekarang adalah tanggal " . date("d-m-Y");
     ?>
 </body>
